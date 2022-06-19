1b705296452c053e0693610c9ea6d8702b809756
Valborian <62020204+Valborian@users.noreply.github.com>
2022-06-19 23:39:53 +0200

    Add files via upload

---
+++ MSEB_19753_Ampel/MSEB_19753_Ampel/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MSEB_19753_Ampel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ampeltimer = new System.Windows.Forms.Timer(this.components);
+            this.Foramationlap = new System.Windows.Forms.Timer(this.components);
+            this.Racemode = new System.Windows.Forms.Timer(this.components);
+            this.Reaktionszeit = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("ISOCPEUR", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(50, 632);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(299, 274);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(366, 698);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Ampeltimer
+            // 
+            this.Ampeltimer.Tick += new System.EventHandler(this.Ampeltimer_Tick);
+            // 
+            // Foramationlap
+            // 
+            this.Foramationlap.Tick += new System.EventHandler(this.Foramationlap_Tick);
+            // 
+            // Racemode
+            // 
+            this.Racemode.Interval = 1;
+            this.Racemode.Tick += new System.EventHandler(this.Racemode_Tick);
+            // 
+            // Reaktionszeit
+            // 
+            this.Reaktionszeit.Interval = 1;
+            this.Reaktionszeit.Tick += new System.EventHandler(this.Reaktionszeit_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MSEB_19753_Ampel.Properties.Resources.BRS_2017_Logo_RZ_CMYK_20170511PNG_Skaliert_1024x388;
+            this.pictureBox3.Location = new System.Drawing.Point(414, 740);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(363, 166);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.BackgroundImage = global::MSEB_19753_Ampel.Properties.Resources.Pedal;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(846, 632);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 274);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Throttle";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MSEB_19753_Ampel.Properties.Resources.RedbullSketch_F1;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 449);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(273, 151);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MSEB_19753_Ampel.Properties.Resources.FerrariSketch_F1;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 300);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1112, 910);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("ISOCPEUR", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Reaktionszeitsimulator made by M.S. 19753";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Ampeltimer;
+        private System.Windows.Forms.Timer Foramationlap;
+        private System.Windows.Forms.Timer Racemode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer Reaktionszeit;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ MSEB_19753_Ampel/MSEB_19753_Ampel/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace MSEB_19753_Ampel
+{
+    public partial class Form1 : Form
+    {
+
+        //deklarieren
+        private int sek;    //um die Ampel in Abhängikeit von Sekunden zu timen
+        int zufallszahl;    //Zufallszahl für zufälliges einschalten der grünen Ampel
+        private bool buttonDruck;   //zum überprüfen ob der throttle Button gedrückt worden ist
+        private int reactTime;  //um dem Computer eine künstliche Reaktionszeit zu geben
+        private int react = 600;   //um Prozess nach einer gewissen Zeit zurückzusetzen
+        int counterRedbull, counterFerrari; //Siegerehrungsvariablen 
+
+
+        //Ampelvariablen
+        private int hoehe = 65;
+        private int breite = 65;
+        private int xKoordinate1 = 900;
+        private int xKoordinate2 = 700;
+        private int xKoordinate3 = 500;
+        private int xKoordinate4 = 300;
+        private int xKoordinate5 = 100;
+        private int yKoordinate1 = 50;
+        private int yKoordinate2 = 130;
+        private int yKoordinate3 = 210;
+
+        //Reaktionszeit
+        bool reaktion;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Du willst wissen ob du es drauf hast F1 Fahrer zu sein? \nSetze dich ins Auto und drücke Throttle wenn alle Ampeln grün aufleuchten \nSchlage Leclerc in einem 1 vs 1 um dir die erste Kurve zu sichern!";
+            MessageBox.Show(message);
+
+            //Bild Anfangswert setzen und picturebox Bild zuweisen
+            //12 279
+            pictureBox1.Location = new Point(-200, 279);
+            pictureBox1.Image = Properties.Resources.FerrariSketch_F1;
+
+            // 12 417 Startposition
+            pictureBox2.Location = new Point(-200, 417);
+            pictureBox2.Image = Properties.Resources.RedbullSketch_F1;
+
+            //Zufallszahl generieren für Startsequenz (grünes Leuchten)
+            var rand = new Random();
+            zufallszahl = rand.Next(4, 10);
+            ////zufallszahl testen
+            //label1.visible = true;
+            //label1.Text = zufallszahl.ToString();
+
+
+            //Autos in Position bringen 
+            Foramationlap.Start();
+            Ampeltimer.Stop();
+            Racemode.Stop();
+            reactTime = 0;
+
+            buttonDruck = false;
+
+            reaktion = false;
+
+            //Am Anfang im Rennen alle Lichter rot an
+            rotesLichtan();
+
+            //sek wieder auf null setzen um Prozess neu zu starten
+            sek = 0;
+
+            //Siegerehrungsvariablen wieder auf null setzen
+            counterRedbull = 0;
+            counterFerrari = 0;
+
+            //Timer konfigurieren 
+            Racemode.Interval = 1;
+
+            Ampeltimer.Interval = 1000;
+
+            Foramationlap.Interval = 1;
+
+            Reaktionszeit.Interval = 200;
+        }
+
+        private void Ampeltimer_Tick(object sender, EventArgs e)
+        {
+            sek++;
+
+            //macht Licht in Abhängigkeit von Sekunden aus und grünes Licht an
+            if (sek == 1) Lichtaus();
+            else if (sek == 2) Lichtaus();
+            else if (sek == 3) Lichtaus();
+            else if (sek == zufallszahl)
+            {
+                gruenesLichtan();
+                Ampeltimer.Stop();
+                Racemode.Start();
+                Reaktionszeit.Start();
+            }
+        }
+
+        private void Foramationlap_Tick(object sender, EventArgs e)
+        {
+            //prüft mit jedem Tick die Position der PictureBox
+            Control control = this.pictureBox1;
+            Point controllocation = control.Location;
+
+            if (controllocation.X == 12)
+            {
+                Foramationlap.Stop();
+                Ampeltimer.Start();
+            }
+            else
+            {
+                pictureBox1.Location = new System.Drawing.Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                pictureBox2.Location = new System.Drawing.Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+            }
+        }
+
+        private void Racemode_Tick(object sender, EventArgs e)
+        {
+
+            reactTime++;
+
+            //Wenn Throttle gedrückt wird bewegt sich das Spieler Auto
+            if (buttonDruck)
+            {
+                pictureBox2.Location = new System.Drawing.Point(pictureBox2.Location.X + 7, pictureBox2.Location.Y);
+                counterRedbull++;
+            }
+
+            //Computergesteuertes Auto fährt los; 200 ms ist die durchschnittliche Reaktionszeit eines F1 Fahrers
+            if (reaktion == true)
+            {
+                pictureBox1.Location = new System.Drawing.Point(pictureBox1.Location.X + 7, pictureBox1.Location.Y);
+                Reaktionszeit.Stop();
+                counterFerrari++;
+            }
+
+            //react = 700
+            if (reactTime == react)
+            {
+                Racemode.Stop();
+                if (counterRedbull > counterFerrari)
+                {
+                    MessageBox.Show("Gewonnen, das restliche Rennen hälst du die Führung mithilfe des stärksten F1 Boxenstopp Teams der Welt!");
+                }
+                else MessageBox.Show("Verloren, Christian Horner ist nicht begeistert");
+
+                Lichtaus();
+                //Bild Anfangswert setzen 
+                pictureBox1.Location = new Point(-200, 279);
+                pictureBox1.Image = Properties.Resources.FerrariSketch_F1;
+
+                pictureBox2.Location = new Point(-200, 417);
+                pictureBox2.Image = Properties.Resources.RedbullSketch_F1;
+
+            }
+
+        }
+
+        //Tickt mit 200 ms -> Reaktionszeit F1 Fahrer
+        private void Reaktionszeit_Tick(object sender, EventArgs e)
+        {
+            reaktion = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonDruck = true;
+        }
+
+
+
+        /// <summary>
+        /// Ampel konfigurieren 
+        /// </summary>
+
+
+        //rotes Licht konfigurieren 
+        public void rotesLichtan()
+        {
+
+            SolidBrush Pinsel = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            System.Drawing.Graphics Kreis;
+            Kreis = this.CreateGraphics();
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate3, breite, hoehe));
+
+        }
+
+        //gruenes Licht konfigurieren
+        public void gruenesLichtan()
+        {
+            SolidBrush Pinsel = new System.Drawing.SolidBrush(System.Drawing.Color.LawnGreen);
+            System.Drawing.Graphics Kreis;
+            Kreis = this.CreateGraphics();
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate1, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate2, breite, hoehe));
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate3, breite, hoehe));
+        }
+
+        //Licht aus konfigurieren
+        public void Lichtaus()
+        {
+            SolidBrush Pinsel = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            System.Drawing.Graphics Kreis;
+            Kreis = this.CreateGraphics();
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate1, breite, hoehe));
+            if (sek == 2) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate2, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate2, breite, hoehe));
+            if (sek == 3) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate3, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate1, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate1, breite, hoehe));
+            if (sek == 2) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate2, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate2, breite, hoehe));
+            if (sek == 3) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate3, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate2, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate1, breite, hoehe));
+            if (sek == 2) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate2, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate2, breite, hoehe));
+            if (sek == 3) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate3, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate3, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate1, breite, hoehe));
+            if (sek == 2) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate2, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate2, breite, hoehe));
+            if (sek == 3) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate3, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate4, yKoordinate3, breite, hoehe));
+
+            Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate1, breite, hoehe));
+            if (sek == 2) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate2, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate2, breite, hoehe));
+            if (sek == 3) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate3, breite, hoehe));
+            else if (reactTime == react) Kreis.FillEllipse(Pinsel, new Rectangle(xKoordinate5, yKoordinate3, breite, hoehe));
+        }
+
+    }
+}
